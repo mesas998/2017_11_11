cd71a26a539412bd45d26f04d8a1f59d7f4d812e
mesas977 <michael.sweeney303@gmail.com>
2017-08-14 16:28:23 -0600

    google verification

--- epa7658577/urls.py
+++ epa7658577/urls.py
@@ -36,5 +36,5 @@ urlpatterns = [
             user_urls,
             app_name='user',
             namespace='dj-auth')),
-    url(r'^googled12693e979b29607\.html$', lambda r: HttpResponse("googled12693e979b29607.html")),
+    url(r'^googled12693e979b29607\.html$', lambda r: HttpResponse("google-site-verification: googled12693e979b29607.html", mimetype="text/plain")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
