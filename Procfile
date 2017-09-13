9c26e21538ccf4c2d5824a4e0ffac4a732aee223
mesas977 <michael.sweeney303@gmail.com>
2017-09-13 10:04:05 -0600

    modify Procfile and wsgi.py for logging on heroku:wq

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: gunicorn --env DJANGO_SETTINGS_MODULE=epa7658577.settings epa7658577.wsgi --log-file -
+web: gunicorn --env DJANGO_SETTINGS_MODULE=epa7658577.settings epa7658577.wsgi --log-file=-
+
--- epa7658577/wsgi.py
+++ epa7658577/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+import logging
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "epa7658577.settings")
 
 application = get_wsgi_application()
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(name)s %(levelname)-8s %(message)s",
+)
