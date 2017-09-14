4fd0705789c99f3fe828367872dcfe39656f03ed
mesas977 <michael.sweeney303@gmail.com>
2017-09-14 14:45:40 -0600

    filter out heroku logs

--- epa7658577/log_filters.py
+++ epa7658577/log_filters.py
@@ -5,8 +5,16 @@ from pprint import pprint
 class ManagementFilter(Filter):
 
     def filter(self, record):
+        LOGS_TO_SUPPRESS = [
+            'heroku',
+            'whatever'
+        ]
+        return not any([word in record.getMessage() for word in LOGS_TO_SUPPRESS])
+        """
+        return not record.getMessage().contains('heroku')
         if (hasattr(record, 'funcName')
                 and record.funcName == 'execute'):
             return False
         else:
             return True
+        """
--- epa7658577/settings.py
+++ epa7658577/settings.py
@@ -170,6 +170,7 @@ LOGGING = {
     },
 }
 """
+
 LOGGING = { 
     'version': 1,
     'disable_existing_loggers': False,
