05d6c915ae42805519f8ff23b47a6d36d7a20001
mesas977 <michael.sweeney303@gmail.com>
2017-09-27 12:06:32 -0600

    add charge and arrested_date fields

--- change_report.py
+++ change_report.py
@@ -1,12 +1,14 @@
 from nutr.models import POC
 for n in POC.audit_log.values():
-  #rint (n)
-  #rint ("n is a ",type(n))
-  #rint ("let's see what's in that dictionary: ")
-  #or k in n:
-    #rint (k,":",n[k])
-  #rint (n['slug'],' created or updated by ',n['modified_by_id'],n['created'])
-  #rint ("%d was created by %d             %s" % (n['id'],n['modified_by_id'],n['created']))
   print ("%d	%s	%s	%s	%s	%d" %  (n['id'], n['action_type'], \
     n['created_by_id'], n['action_date'], n['created_by_id'], n['tag_id']))
-  #n['created_by_id'])
+
+"""
+  print (n)
+  print ("n is a ",type(n))
+  print ("let's see what's in that dictionary: ")
+  for k in n:
+    print (k,":",n[k])
+  print (n['slug'],' created or updated by ',n['modified_by_id'],n['created'])
+  print ("%d was created by %d             %s" % (n['id'],n['modified_by_id'],n['created']))
+"""
--- nutr/models.py
+++ nutr/models.py
@@ -75,10 +75,10 @@ class POC(TimeStampedModel, AuthStampedModel):
     )
     status = models.CharField(null=True,max_length=1, choices=STATUS_CHOICES)
     #udge = models.TextField(blank=True,max_length=255,null=True,verbose_name="Presiding Judge")
-    #rrested_date = models.DateField(blank=True,null=True)
     #eleased_date = models.DateField(blank=True,null=True)
     #rial_date = models.DateField(blank=True,null=True)
-    #harge = models.TextField(blank=True,max_length=255,null=True)
+    arrested_date = models.DateField(blank=True,null=True)
+    charge = models.TextField(blank=True,max_length=255,null=True)
     audit_log = AuditLog()
 
     def save(self, *args, **kwargs):
